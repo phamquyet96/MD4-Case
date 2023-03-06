09bad7c793abfd15dbaab88c20a1097987fbb9a7
phamquyet9688 <117346649+phamquyet9688@users.noreply.github.com>
2023-03-06 10:58:38 +0700

    Delete dist directory

--- dist/src/middleware/jwtauth.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const jwtauth: (req: any, res: any, next: any) => Promise<any>;
--- dist/src/router/product.router.d.ts
+++
@@ -1,2 +0,0 @@
-declare const productRoutes: import("express-serve-static-core").Router;
-export default productRoutes;
